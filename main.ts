404ae661bd4391f30676410d9b963f7d07735c7d
Mohammed-Afheej <mohammed.afheej@mths.ca>
2024-10-17 10:43:08 -0400

    Edited the comment

--- main.ts
+++ main.ts
@@ -5,4 +5,20 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+let distanceToObject:number = 0
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+// find sistance from sonar
+input.onButtonPressed(Button.A, function (){
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
+})
+
